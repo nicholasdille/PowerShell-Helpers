28d2690e4e1eb21ab4960d2a6bc425eda75a6d5d
Nicholas Dille <nicholasdille@users.noreply.github.com>
2017-09-22 08:55:25 +0200

    New helper

---
+++ Helper/Public/Find-DockerImageTag.ps1
@@ -0,0 +1,41 @@
+﻿function Find-DockerImageTag {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Image
+        ,
+        [Parameter(Mandatory)]
+        [switch]
+        $Raw
+    )
+
+    begin {
+        if ($Image -notlike '*/*') {
+            $Image = "library/$Image"
+        }
+
+        $Tags = @()
+    }
+
+    process {
+        $NextPage = "https://registry.hub.docker.com/v2/repositories/$Image/tags"
+        while ($NextPage) {
+            $Result = Invoke-RestMethod -UseBasicParsing -Uri "$NextPage"
+    
+            $Tags += $Result.results
+
+            $NextPage = $Result.next
+        }
+    }
+
+    end {
+        if ($Raw) {
+            $Tags | Sort-Object -Property name
+
+        } else {
+            $Tags.name | Sort-Object
+        }
+    }
+}
