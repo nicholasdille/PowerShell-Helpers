61c139afb0e3921b8462d190813fb64271233e06
Nicholas Dille <nicholas.dille@mailbox.org>
2018-03-30 12:53:25 +0200

    Added Get-BasicAuthentication to enable authenticated web requests without WebRequest

---
+++ Helpers/Public/Get-BasicAuthentication.ps1
@@ -0,0 +1,16 @@
+function Get-BasicAuthentication {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $User
+        ,
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Token
+    )
+
+    ConvertTo-Base64 -Data "$($User):$Token" -Encoding ASCII
+}
